233ded3310b0dd57e4ae3a08aa3da57ac882b0c1
Dev Bolton <jabolton939@gmail.com>
2026-08-13 12:34:34 +0000

    fixed room param in chat side bar not pulling room channels

--- front/src/components/hub/hubbars.jsx
+++ front/src/components/hub/hubbars.jsx
@@ -4,18 +4,19 @@ import '../../styles/hub/SideBar.css'
 import '../../styles/hub/ControlBar.css'
 import CreateChannel from '../chat/CreateChannel'
 
-// TODO: fix list of channels now showing on refresh
-function ChatSideBar({room}) {
+function ChatSideBar({ room }) {
     const { roomId } = useParams()
-    const [channels, setChannels] = useState([])
+    const [ refreshedChannels, setRefreshedChannels] = useState(null)
+
+    const channels = refreshedChannels ?? room.channels ?? []
 
     const refreshChannels = () => {
-    const cached = localStorage.getItem('current_room')
-    if (cached) {
-        const room = JSON.parse(cached)
-        setChannels(room.channels || [])
+        const cached = localStorage.getItem('current_room')
+        if (cached) {
+            const cachedRoom = JSON.parse(cached)
+            setRefreshedChannels(cachedRoom.channels || [])
+        }
     }
-}
 
     return (
         <div id="chat" className="sidebar">
@@ -32,7 +33,7 @@ function ChatSideBar({room}) {
                     ))}
                 </ul>
             </ul>
-            <CreateChannel roomId={roomId} onChannelCreated={refreshChannels}/>
+            <CreateChannel roomId={roomId} onChannelCreated={refreshChannels} />
         </div>
     )
 }
