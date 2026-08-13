609c8c6852af02bee5b8b2cf1e2a05093e551574
Dev Bolton <jabolton939@gmail.com>
2026-08-13 16:34:09 +0000

    Minor changes to room profile

--- front/src/components/hub/hubbars.jsx
+++ front/src/components/hub/hubbars.jsx
@@ -70,8 +70,8 @@ function ControlBar(){
     return(
         <div className="control-bar">
             <div className='button-container'>
-                <a href="/createroom"><button>+</button></a>
-                <a><button>F</button></a>
+                <a href="/createroom"><button>Create server</button></a>
+                <a><button>Friends</button></a>
             </div>
             <form>
                 <input
@@ -79,7 +79,7 @@ function ControlBar(){
                 ></input>
             </form>
             <div className='button-container'>
-                <a href='/me'><button>A</button></a>
+                <a href='/me'><button>Profile</button></a>
             </div>
         </div>
     )
